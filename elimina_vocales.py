fffa928f8769baeb35c2f91c905cf0e48e99a93a
addolfo <financieras@gmail.com>
2020-08-30 20:27:20 +0200

    Eliminar vocales de un texto y la letra o se cambia por un cero

---
+++ elimina_vocales.py
@@ -0,0 +1,22 @@
+def procesa(texto):
+    vocales=('a','e','i','o','u')
+    texto=texto.replace('o','0')
+    texto=texto.replace('O','0')
+    for vocal in vocales:
+        texto=texto.replace(vocal,'')
+        texto=texto.replace(vocal.upper(),'')
+    return texto
+texto="Viajaremos a Oviedo, Alicante o a Bilbao."
+print(procesa(texto))
+def procesa2(texto):
+    texto=texto.replace('a','')
+    texto=texto.replace('e','')
+    texto=texto.replace('i','')
+    texto=texto.replace('o','0')
+    texto=texto.replace('u','')
+    texto=texto.replace('A','')
+    texto=texto.replace('E','')
+    texto=texto.replace('I','')
+    texto=texto.replace('O','0')
+    texto=texto.replace('U','')
+    return texto
