35c96a7a804eca3191fd477d60206343ca2cf645
addolfo <financieras@gmail.com>
2020-08-30 20:58:26 +0200

    Listas antes de saber estructuras

---
+++ listas.py
@@ -0,0 +1,13 @@
+#Una lista es un conjunto iterable de datos ordenados que van entre corchetes []
+[]                                          #una lista vacía
+[10, 15, 12]	                                #una lista de tres elementos con números enteros
+['azul', 'rojo', 'verde', 'amarillo']       #una lista con strings
+[4, 'lunes', 3.14, True, None]              #admite cualquier valor
+comida=['spagueti','pollo','tarta']        #asignación a una variable
+print(comida)                               # ['spagueti', 'pollo', 'tarta']
+comida[0]                                   #'spagueti'   se comienza por el índice=0             
+comida[1]                                   #'pollo'
+comida[2]                                   #'tarta'
+comida[0]='espagueti'                       #reemplazando un elemento
+print(comida)                               # ['espagueti', 'pollo', 'tarta']
+
