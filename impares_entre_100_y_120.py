2e0d26096ed7b0f56f549dc678811ac367398621
addolfo <financieras@gmail.com>
2020-09-01 14:10:39 +0200

    Varios métodos

--- impares_entre_100_y_120.py
+++ impares_entre_100_y_120.py
@@ -1,25 +1,17 @@
-#Generar números impares entre 100 y 120
-lista=[]
-for i in range(101,120):
-    lista.append(i)
-    i=i+2
-print(lista)
-#########################################
-lista=[]
+lista=[] #Generar números impares entre 100 y 120   #Método 1
 for i in range(101,120,2):
     lista.append(i)
 print(lista)
-#########################################
 lista=[]
-i=101
+i=101                                               #Método 2
 while i<120:
     lista.append(i)
     i=i+2
 print(lista)
-#########################################
-lista=[]
+lista=[]                                            #Método 3
 for i in range(100,120):
     if i%2!=0:
         lista.append(i)
 print(lista)
-#########################################
+print([i for i in range(101,120,2)])                #Método 4
+[print(i) for i in range(101,120) if i%2==0]        #Método 5
